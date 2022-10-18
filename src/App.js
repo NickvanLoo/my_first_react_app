0dcae74ba0f680f0ed8abd97136286154fd8547a
NickvanLoo28 <58033512+NickvanLoo@users.noreply.github.com>
2022-10-18 13:08:55 +0200

    My first React app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welkom op mijn 1e React-app.</h1>
+        <p>Dit is een mijn 1e React app.<br></br>
+          Ik heb dit gemaakt om kennis te maken met het React JavaScript Framework.
+        </p>
+      </header>
+      <footer>&copy; 2022 Nick van Loo</footer>
+    </div>
+  );
+}
+
+export default App;
